18120785fea2046ff34c4f20e5aa8d550a5a1f2c
kareem <kareemaboelatta@gmail.com>
2024-05-22 03:58:54 +0300

    move auth things to auth module to separate all of it.

---
+++ home/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+
+}
+
+android {
+    namespace = "com.example.home"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+}
+
+dependencies {
+    implementation(project(":common"))
+
+}
